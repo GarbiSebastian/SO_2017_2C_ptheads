f45663ec51e4d1d2afac8026e02179903009c29b
GarbiSebastian <garbyseba@gmail.com>
2017-08-27 20:01:18 -0300

    ConcurrentHashMap par que compile

---
+++ ConcurrentHashMap.cpp
@@ -0,0 +1,33 @@
+using namespace std;
+
+#include "ConcurrentHashMap.hpp"
+
+void ConcurrentHashMap::addAndInc(string key){
+		
+}
+
+bool ConcurrentHashMap::member(string key){
+	return true;
+}
+
+item ConcurrentHashMap::maximum(unsigned int nt){
+	return make_pair("Hola",3);
+}
+
+ConcurrentHashMap ConcurrentHashMap::count_words(string arch){
+	ConcurrentHashMap a;
+	return a;
+}
+
+ConcurrentHashMap ConcurrentHashMap::count_words(list<string> archs){
+	ConcurrentHashMap a;
+	return a;
+}
+ConcurrentHashMap ConcurrentHashMap::count_words(unsigned int n, list<string> archs){
+	ConcurrentHashMap a;
+	return a;
+}
+
+item ConcurrentHashMap::maximum(unsigned int p_archivos, unsigned int p_maximos, list<string> archs){
+	return make_pair("Hola",3);
+}
---
+++ ConcurrentHashMap.hpp
@@ -0,0 +1,28 @@
+#ifndef CONCURRENT_HASHMAP_H__
+#define CONCURRENT_HASHMAP_H__
+
+using namespace std;
+
+#include <atomic>
+#include <string>
+#include "ListaAtomica.hpp"
+#include <list>
+
+typedef pair<string, unsigned int> item;
+
+class ConcurrentHashMap{
+	public:
+		Lista<item>* tabla[26];
+
+	public:
+		void addAndInc(string key);
+		bool member(string key);
+		item maximum(unsigned int nt);
+		static ConcurrentHashMap count_words(string arch);
+		static ConcurrentHashMap count_words(list<string> archs);
+		static ConcurrentHashMap count_words(unsigned int n, list<string> archs);
+		static item maximum(unsigned int p_archivos, unsigned int p_maximos, list<string> archs);
+		
+};
+
+#endif /* CONCURRENT_HASHMAP_H__ */
